a29147a8e025fa6e52e8e861b909c0af6e6725b2
Allan Dominguez <allan.dominguez@students.mq.edu.au>
2017-11-05 21:50:50 +1100

    Implemented GA - GA fitness of 89 - Attempting to increase generated fitness - reforming the population selection method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import axelrod as axl
 from deap import base, creator, tools
-import random
+import random, re
 
 # --- AXELROD EXAMPLE ---
 # players = (axl.Cooperator(), axl.Alternator())
@@ -39,21 +39,20 @@ import random
 
 # Fitness Function
 def fit_func(individual):
-    if len(individual) == 64:
-        return 0
+    return play_ind(individual),
 
 
 # playing a bit string strategy
 def play_ind(bits):
     score = 0
     for p1, p2 in zip(bits[0::2], bits[1::2]):
-        if p1 == '1':
-            if p2 == '1':
+        if p1 == 1:
+            if p2 == 1:
                 score += 3
             else:
                 score += 0
         else:
-            if p2 == '1':
+            if p2 == 1:
                 score += 5
             else:
                 score += 1
@@ -82,12 +81,94 @@ def create_toolbox(num_bits):
     toolbox.register("evaluate", fit_func)
 
     # Register the crossover operator
-    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mate", tools.cxUniformPartialyMatched, indpb=0.5)
 
     # Register a mutation operator
     toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
 
     # Operator for selecting individuals for breeding
-    toolbox.register("select", tools.selTournament, tournsize=3)
+    toolbox.register("select", tools.selRoulette)   # based on fitness
 
     return toolbox
+
+
+if __name__ == "__main__":
+    num_bits = 64
+
+    toolbox = create_toolbox(num_bits)
+
+    random.seed(7)
+
+    population = toolbox.population(n=12)
+
+    prob_cross, prob_mute = 0.5, 0.2
+
+    num = input("How many generations? ")
+
+    num_generations = int(re.search(r'\d+', num).group())
+
+    print('\nStarting the evolution process')
+
+    # Evaluate the entire population
+    fitnesses = list(map(toolbox.evaluate, population))
+    for ind, fit in zip(population, fitnesses):
+        ind.fitness.values = fit
+
+    print('\nEvaluated', len(population), 'individuals')
+
+    # Iterate through generations
+    for g in range(num_generations):
+        print("\n===== Generation", g)
+
+        # Select the next generation individuals
+        offspring = toolbox.select(population, len(population))
+        print(len(offspring))
+
+        # Clone the selected individuals
+        offspring = list(map(toolbox.clone, offspring))
+
+        # Apply crossover and mutation on the offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            # Cross two individuals
+            if random.random() < prob_cross:
+                toolbox.mate(child1, child2)
+
+                # "Forget" the fitness values of the children
+                del child1.fitness.values
+                del child2.fitness.values
+
+        # Apply mutation
+        for mutant in offspring:
+            # Mutate an individual
+            if random.random() < prob_mute:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        print('Re-evaluated', len(invalid_ind), 'individuals')
+
+        # The population is entirely replaced by the offspring
+        population[:] = offspring
+
+        # Gather all the fitnesses in one list and print the stats
+        fits = [ind.fitness.values[0] for ind in population]
+
+        length = len(population)
+        mean = sum(fits) / length
+        sum2 = sum(x * x for x in fits)
+        std = abs(sum2 / length - mean ** 2) ** 0.5
+
+        print('Min =', min(fits), ', Max =', max(fits))
+        print('Average =', round(mean, 2), ', Standard deviation =',
+              round(std, 2))
+
+    print("\n==== End of evolution")
+
+    best_ind = tools.selBest(population, 1)[0]
+    print('\nBest individual:\n', best_ind)
+    print('\nFitness:', play_ind(best_ind))
